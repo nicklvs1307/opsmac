c925c2030bd23b86d45acc1ce03df82a36983916
Lucas <lucas.niquele.ln@gmail.com>
2025-08-11 18:00:31 -0300

    feat: atualiza projeto feedeliza

--- migrations/20250811113834-add-rfv-score-to-customers.js
+++ migrations/20250811113834-add-rfv-score-to-customers.js
@@ -3,19 +3,25 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('customers', 'rfv_score', {
-      type: Sequelize.JSONB,
-      allowNull: true,
-      defaultValue: {
-        recency: null,
-        frequency: null,
-        monetary: null
-      },
-      comment: 'Pontuação RFV (Recência, Frequência, Valor)'
-    });
+    const tableDefinition = await queryInterface.describeTable('customers');
+    if (!tableDefinition.rfv_score) {
+      await queryInterface.addColumn('customers', 'rfv_score', {
+        type: Sequelize.JSONB,
+        allowNull: true,
+        defaultValue: {
+          recency: null,
+          frequency: null,
+          monetary: null
+        },
+        comment: 'Pontuação RFV (Recência, Frequência, Valor)'
+      });
+    }
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('customers', 'rfv_score');
+    const tableDefinition = await queryInterface.describeTable('customers');
+    if (tableDefinition.rfv_score) {
+      await queryInterface.removeColumn('customers', 'rfv_score');
+    }
   }
 };

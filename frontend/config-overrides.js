4d98d0d9c4ad08702096ad7b7df5f4242126b023
Lucas <lucas.niquele.ln@gmail.com>
2025-09-06 13:36:50 -0300

    feat: atualiza projeto feedeliza

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -22,5 +22,26 @@ module.exports = function override(config) {
     '@mui/icons-material': path.resolve(__dirname, 'node_modules/@mui/icons-material/index.js'),
   };
 
+  config.module.rules = config.module.rules.map(rule => {
+    if (rule.oneOf) {
+      return {
+        ...rule,
+        oneOf: rule.oneOf.map(oneOfRule => {
+          if (oneOfRule.loader && oneOfRule.loader.includes('babel-loader')) {
+            return {
+              ...oneOfRule,
+              include: [
+                oneOfRule.include,
+                path.resolve(__dirname, 'node_modules'),
+              ],
+            };
+          }
+          return oneOfRule;
+        }),
+      };
+    }
+    return rule;
+  });
+
   return config;
 };

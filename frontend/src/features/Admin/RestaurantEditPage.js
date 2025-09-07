78062d801aa93ac091f80a4e15eb242ad3ca716a
Lucas <lucas.niquele.ln@gmail.com>
2025-09-07 17:26:18 -0300

    feat: atualiza projeto feedeliza

--- frontend/src/features/Admin/RestaurantEditPage.js
+++ frontend/src/features/Admin/RestaurantEditPage.js
@@ -145,7 +145,9 @@ const RestaurantEditPage = () => {
         setChildren(newSelected[moduleId], checked);
     }
 
+    console.log('DEBUG: newSelected after direct change (before updateParentStates):', JSON.stringify(newSelected, null, 2));
     const updatedState = updateParentStates(newSelected, permissionTree);
+    console.log('DEBUG: updatedState after updateParentStates:', JSON.stringify(updatedState, null, 2));
     setSelectedPermissions(updatedState);
   };
 

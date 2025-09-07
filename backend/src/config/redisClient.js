433ebf64580aa5ceff79d8214d677eb1fc3b1561
Lucas <lucas.niquele.ln@gmail.com>
2025-09-07 09:12:36 -0300

    feat: atualiza projeto feedeliza

--- backend/src/config/redisClient.js
+++ backend/src/config/redisClient.js
@@ -4,7 +4,7 @@ const redisClient = redis.createClient({
   url: process.env.REDIS_URL || 'redis://localhost:6379', // Use environment variable for Redis URL
 });
 
-redisClient.on('connect', () => { /* console.log('Redis Client Connected') */ }); // Commented out
+redisClient.on('connect', () => console.log('Redis Client Connected'));
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
 // Connect to Redis

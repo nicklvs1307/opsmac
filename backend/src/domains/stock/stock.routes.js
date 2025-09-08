0a7c6f16b8c983998923d1fa31e9b36cb2489d28
Lucas <lucas.niquele.ln@gmail.com>
2025-09-08 13:44:08 -0300

    feat: atualiza projeto feedeliza

--- backend/src/domains/stock/stock.routes.js
+++ backend/src/domains/stock/stock.routes.js
@@ -1,17 +1,20 @@
 const express = require('express');
-const { auth } = require('../../middleware/authMiddleware');
+
 const requirePermission = require('../../middleware/requirePermission');
 const stockController = require('./stock.controller');
 const {
     createStockMovementValidation
 } = require('./stock.validation');
 
-const router = express.Router();
+module.exports = (db) => {
+  const { auth } = require('../../middleware/authMiddleware')(db);
+  const router = express.Router();
 
-// Rotas de Estoque
-router.get('/dashboard', auth, requirePermission('stock', 'read'), stockController.getDashboardData);
-router.get('/', auth, requirePermission('stock', 'read'), stockController.getAllStocks);
-router.post('/move', auth, requirePermission('stock', 'update'), createStockMovementValidation, stockController.createStockMovement);
-router.get('/history/:productId', auth, requirePermission('stock', 'read'), stockController.getStockHistory);
+  // Rotas de Estoque
+  router.get('/dashboard', auth, requirePermission('stock', 'read'), stockController.getDashboardData);
+  router.get('/', auth, requirePermission('stock', 'read'), stockController.getAllStocks);
+  router.post('/move', auth, requirePermission('stock', 'update'), createStockMovementValidation, stockController.createStockMovement);
+  router.get('/history/:productId', auth, requirePermission('stock', 'read'), stockController.getStockHistory);
 
-module.exports = router;
+  return router;
+};

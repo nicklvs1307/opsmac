46e5cbd44efc1a5eac399c82fa6e44346560d96d
Lucas <lucas.niquele.ln@gmail.com>
2025-09-08 13:46:46 -0300

    feat: atualiza projeto feedeliza

--- backend/src/domains/tables/tables.routes.js
+++ backend/src/domains/tables/tables.routes.js
@@ -1,15 +1,18 @@
 const express = require('express');
-const { auth } = require('../../middleware/authMiddleware');
+
 const requirePermission = require('../../middleware/requirePermission');
 const tablesController = require('./tables.controller');
 const {
     createTableValidation
 } = require('./tables.validation');
 
-const router = express.Router();
+module.exports = (db) => {
+  const { auth } = require('../../middleware/authMiddleware')(db);
+  const router = express.Router();
 
-// Rotas de Mesas
-router.post('/', auth, requirePermission('tables', 'create'), createTableValidation, tablesController.createTable);
-router.get('/', auth, requirePermission('tables', 'read'), tablesController.listTables);
+  // Rotas de Mesas
+  router.post('/', auth, requirePermission('tables', 'create'), createTableValidation, tablesController.createTable);
+  router.get('/', auth, requirePermission('tables', 'read'), tablesController.listTables);
 
-module.exports = router;
+  return router;
+};

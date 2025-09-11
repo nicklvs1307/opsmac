b02717316214e624e520a3572278d56be85151c7
Lucas <lucas.niquele.ln@gmail.com>
2025-09-11 16:47:21 -0300

    feat: atualiza projeto feedeliza

--- backend/src/utils/safeRouter.js
+++ backend/src/utils/safeRouter.js
@@ -1,37 +1,17 @@
 const express = require("express");
-const METHODS = ["get", "post", "put", "patch", "delete", "all", "use"];
+const METHODS = ["get", "post", "put", "patch", "delete", "all"];
 
 function safeRouter() {
   const router = express.Router();
   for (const m of METHODS) {
     const orig = router[m].bind(router);
-    router[m] = (...args) => {
-      let path = undefined;
-      let handlers = args;
-
-      if (m === 'use' && typeof args[0] === 'string') {
-        path = args[0];
-        handlers = args.slice(1);
-      } else if (m !== 'use' && typeof args[0] === 'string') {
-        path = args[0];
-        handlers = args.slice(1);
-      } else {
-        // If it's 'use' and the first arg is not a string, it's a middleware function without a path
-        // For other methods, if the first arg is not a string, it's an error (handled by Express itself)
-        handlers = args;
-      }
-
+    router[m] = (path, ...handlers) => {
       handlers.forEach((h, i) => {
         if (typeof h !== "function") {
-          throw new Error(`Router.${m}(${path ? `"${path}"` : ''}) handler[${i}] é ${typeof h}; expected function`);
+          throw new Error(`Router.${m}("${path}") handler[${i}] é ${typeof h}; expected function`);
         }
       });
-
-      if (path !== undefined) {
-        return orig(path, ...handlers);
-      } else {
-        return orig(...handlers);
-      }
+      return orig(path, ...handlers);
     };
   }
   return router;

932c216770e4a2cb0363669ee5297ac30d3e4ccf
Lucas <lucas.niquele.ln@gmail.com>
2025-09-11 16:52:39 -0300

    feat: atualiza projeto feedeliza

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,7 +10,8 @@ module.exports = (db) => {
     
     // Apply the restaurant context middleware globally to all routes handled by this router
     console.log('Type of getRestaurantContextMiddleware():', typeof getRestaurantContextMiddleware());
-    router.use(getRestaurantContextMiddleware()); // Call it as a function to get the middleware
+    const restaurantContextMiddleware = getRestaurantContextMiddleware();
+    router.use(restaurantContextMiddleware); // Call it as a function to get the middleware
     router.use(auth.auth); // Apply auth middleware globally
 
     const domainsDir = path.join(__dirname, "..", "src", "domains");

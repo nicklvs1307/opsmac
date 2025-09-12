a8c3d53a7db2ff9dbd07ebe2240bcb876b8caf04
Lucas <lucas.niquele.ln@gmail.com>
2025-09-12 09:08:58 -0300

    feat: atualiza projeto feedeliza

--- backend/src/middleware/getRestaurantContextMiddleware.js
+++ backend/src/middleware/getRestaurantContextMiddleware.js
@@ -1,6 +1,8 @@
 module.exports = () => {
   return (req, res, next) => {
     req.context = req.context || {};
+    // Attempt to get restaurantId from various sources
+    req.context.restaurantId = req.params.restaurantId || req.query.restaurantId || req.body.restaurantId;
     next();
   };
 };

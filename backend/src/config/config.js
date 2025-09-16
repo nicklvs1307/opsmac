d36bdc06c79cdd281122d96dac865801ef8143d7
Lucas <lucas.niquele.ln@gmail.com>
2025-09-16 15:03:54 -0300

    feat: atualiza projeto feedeliza

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production" && process.env.DB_PASSWORD_FILE) {
   }
 }
 
-module.exports = {
+export default {
   development: {
     username: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD || "123456789",

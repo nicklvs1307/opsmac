4821efa650cb886b763f5c3e4e6a71b490b2cb97
Lucas <lucas.niquele.ln@gmail.com>
2025-09-16 16:02:32 -0300

    feat: atualiza projeto feedeliza

--- .sequelizerc.cjs
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-// require('./backend/aliases');
-
-module.exports = {
-  'config': path.resolve('backend', 'src', 'config', 'config.js'),
-  'models-path': path.resolve('backend', 'models'),
-  'seeders-path': path.resolve('backend', 'seeders'),
-  'migrations-path': path.resolve('backend', 'migrations')
-};
